69563df5807f7433fb04b0805c9cae04a6bcb330
Jonah Sullivan <jonah.sullivan@ga.gov.au>
2018-04-19 11:52:19 +1000

    Update geodesic_densifier_dialog.py

--- geodesic_densifier_dialog.py
+++ geodesic_densifier_dialog.py
@@ -21,14 +21,20 @@
 
 import os
 
-from PyQt4 import QtGui, uic
+from PyQt5 import uic
+from PyQt5 import QtWidgets
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'geodesic_densifier_dialog_base.ui'))
 
 
-class GeodesicDensifierDialog(QtGui.QDialog, FORM_CLASS):
+class GeodesicDensifierDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
         super(GeodesicDensifierDialog, self).__init__(parent)
-        self.setupUi(self)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+self.setupUi(self)
