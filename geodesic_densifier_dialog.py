0e4af40ba525bfb6dafa9b07cf39e9b61bc22f33
Jonah Sullivan <jonah.sullivan@ga.gov.au>
2018-04-19 11:20:51 +1000

    updated license blurb

--- geodesic_densifier_dialog.py
+++ geodesic_densifier_dialog.py
@@ -14,9 +14,7 @@
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the Creative Commons 4.0 International License. *
  *                                                                         *
  ***************************************************************************/
 """
